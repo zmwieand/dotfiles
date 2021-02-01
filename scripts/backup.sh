19924d3dcb156f1ef19768ccb8648f1505b468ff
zmwieand <zmwieand@gmail.com>
2021-01-31 19:57:21 -0500

    small fixes to backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,25 +5,31 @@ NAS_USER=$(cat ~/.backuprc | grep "^  user: " | awk '{print $2}')
 MOUNT_LOCATION=$(cat ~/.backuprc | grep "^  mountLocation: " | awk '{print $2}' | /usr/local/bin/gsed "s@\~@${HOME}@g")
 
 # Disconnect from the VPN if necessary
-echo "Killing VPN Connection..."
-sudo pkill -x "openvpn"
+# echo "Killing VPN Connection..."
+# sudo pkill -x "openvpn"
 
 # Mount the NAS
-echo "Mounting ${NAS_SERVER} to ${MOUNT_LOCATION}"
-/sbin/mount_smbfs //${NAS_SERVER}/${NAS_USER} ${MOUNT_LOCATION}
-EXIT_CODE=$?
+NAS_MOUNTED=$(mount | grep /Users/`whoami`/NAS)
+if [[ -z ${NAS_MOUNTED} ]]; then
+  echo "Mounting ${NAS_SERVER} to ${MOUNT_LOCATION}"
+  /sbin/mount_smbfs //${NAS_SERVER}/${NAS_USER} ${MOUNT_LOCATION}
+  EXIT_CODE=$?
 
-# If there is an error mounting fail fast
-if [[ ${EXIT_CODE} -ne 0 ]]; then
-  echo "[Error]: There was an error mounting ${NAS_SERVER} to ${MOUNT_LOCATION}"
-  echo "Exit Code: ${EXIT_CODE}"
-  exit 1
+  # If there is an error mounting fail fast
+  if [[ ${EXIT_CODE} -ne 0 ]]; then
+    echo "[Error]: There was an error mounting ${NAS_SERVER} to ${MOUNT_LOCATION}"
+    echo "Exit Code: ${EXIT_CODE}"
+    exit 1
+  fi
+else
+  echo "NAS already mounted..."
 fi
 
 # Perfrom Backup
-echo "Running Backup..."
+DATE=$(date +"%m/%d/%Y")
+TIME=$(date +"%H:%M:%S")
+echo "Running Backup for ${DATE} at ${TIME}..."
 cd ${HOME}/dev/dotfiles/backup
-echo "CWD: `pwd`"
 /usr/local/bin/pipenv run python -m src.main 2>&1 &
 
 # Make sure the machine does not sleep for the duration of the backup
