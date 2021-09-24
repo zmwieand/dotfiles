b48d862c41e59f1c99002c34c1c1981881a29940
zmwieand <zmwieand@gmail.com>
2021-09-24 11:06:18 -0400

    updating ecr_login.sh with aws cli v2 updates

--- scripts/ecr_login.sh
+++ scripts/ecr_login.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-eval $(aws ecr get-login --no-include-email --region us-east-1)
+# AWS CLI v1
+# eval $(aws ecr get-login --no-include-email --region us-east-1)
+
+# AWS CLI v2
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 294290347293.dkr.ecr.us-east-1.amazonaws.com
