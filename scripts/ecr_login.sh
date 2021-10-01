f8aae2abcc718b88c396d54aba890cfed5673121
zmwieand <zmwieand@gmail.com>
2021-10-01 14:58:35 -0400

    aws cli v2 on ecr login

--- scripts/ecr_login.sh
+++ scripts/ecr_login.sh
@@ -4,4 +4,18 @@
 # eval $(aws ecr get-login --no-include-email --region us-east-1)
 
 # AWS CLI v2
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 294290347293.dkr.ecr.us-east-1.amazonaws.com
+PROFILES=$(cat ~/.aws/config | grep -E "\[.*\]" | sed -e "s/\[//g" | sed -e "s/\]//g" | sed -e "s/profile //g")
+
+for profile_name in ${PROFILES}
+do
+  ACCOUNT_ID=$(cat ~/.aws/config | grep -A 6 -E "\[.*${profile_name}\]" | grep "sso_account_id" | awk '{print $3}')
+
+  # If this is not the default profile, add the --profile arg
+  PROFILE_ARG=""
+  if [[ ${profile_name} != "default" ]]; then
+    PROFILE_ARG="--profile ${profile_name}"
+  fi
+
+  # Login with the proper Profile and Account ID
+  aws ecr get-login-password --region us-east-1 ${PROFILE_ARG} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com
+done
